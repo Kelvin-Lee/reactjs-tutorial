fe15be15b531abad7e14cd92bdbde93f2b37c4d0
Kelvin Lee <Kelvin-Lee@users.noreply.github.com>
2020-02-15 22:03:06 -0800

    Tutorial: "Implementing Time Travel"

--- src/index.js
+++ src/index.js
@@ -48,7 +48,7 @@ class Board extends React.Component {
 class Game extends React.Component {
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -59,7 +59,15 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares,
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0
     });
   }
 
@@ -69,14 +77,14 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null),
       }],
-      xIsNext: true,
+      stepNumber: 0,
+      xIsNext: true
     };
   }
 
   render() {
-
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
@@ -84,7 +92,7 @@ class Game extends React.Component {
         'Go to move #' + move :
         'Go to game start';
       return (
-        <li>
+        <li key={move}>
           <button onClick={() => this.jumpTo(move)}>{desc}</button>
         </li>
       );
